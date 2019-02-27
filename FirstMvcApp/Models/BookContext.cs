628acb560a7fbcaa3b0ec11e8e05927b6e7d2eb9
vKanenkov <vKanenkov@gmail.com>
2019-02-27 15:06:55 +0300

    Initial commit

---
+++ FirstMvcApp/Models/BookContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data.Entity;
+
+namespace FirstMvcApp.Models
+{
+    public class BookContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
