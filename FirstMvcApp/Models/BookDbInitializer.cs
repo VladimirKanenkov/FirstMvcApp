ca08614b45d149cbcb18daececb00a1cf4932f64
vKanenkov <vKanenkov@gmail.com>
2019-03-05 11:42:06 +0300

    Заработало!!!

--- FirstMvcApp/Models/BookDbInitializer.cs
+++ FirstMvcApp/Models/BookDbInitializer.cs
@@ -12,7 +12,7 @@ namespace BookStore.Models
         {
             db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
             db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Price = 180 });
-            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 1500 });
 
             base.Seed(db);
         }
